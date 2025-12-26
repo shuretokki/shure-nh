f45eb053439267a7798b587fe1130f83d0c929ad
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-26 13:06:05 +0700

    init

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{ 
+    description = "Hyprland";
+
+    inputs = {
+        nixpkgs.url = "nixpkgs/nixos-unstable";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs = { nixpkgs, home-manager, ... }: {
+        nixosConfigurations.shure = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+                ./configuration.nix
+                home-manager.nixosModules.home-manager {
+                    home-manager = {
+                        useGlobalPkgs = true;
+                        useUserPackages = true;
+                        users.shure = import ./home.nix;
+                        backupFileExtension = "backup";
+                    };
+                }
+            ];
+        };
+    };
+}
