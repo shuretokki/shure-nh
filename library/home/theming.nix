b7437a5961594da5a7e7bb05f0db253f63322f3d
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-27 21:43:06 +0700

    add gtk styling

--- library/home/theming.nix
+++ library/home/theming.nix
@@ -14,6 +14,31 @@
       package = pkgs.apple-cursor;
       size = 24;
     };
+
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+      gtk-button-images = 1;
+      gtk-menu-images = 1;
+      gtk-enable-event-sounds = 0;
+      gtk-enable-input-feedback-sounds = 0;
+    };
+
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+
+    gtk3.extraCss = ''
+      .background {
+        border-radius: 10px;
+      }
+      window.background.popup {
+        border-radius: 10px;
+        box-shadow: 0 0 10px rgba(0,0,0,0.5);
+      }
+      headerbar {
+        border-radius: 10px 10px 0 0;
+      }
+    '';
   };
 
   home.pointerCursor = {
