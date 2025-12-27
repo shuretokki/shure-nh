ef7f4fd6a347276b0191bbd43c060ac71fdf0385
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-27 17:32:00 +0700

    complete mods

--- library/display/env.nix
+++
@@ -1,7 +0,0 @@
-# https://wiki.hypr.land/Configuring/Environment-variables/
-{
-  env = [
-    "XCURSOR_SIZE,24"
-    "HYPRCURSOR_SIZE,24"
-  ];
-}
--- library/display/swayosd.nix
+++
@@ -1,3 +0,0 @@
-{ pkgs, ... }: {
-  services.swayosd.enable = true;
-}
