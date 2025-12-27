d59ab9390d34318116ef636aacd72875bd9a69e8
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-27 21:43:05 +0700

    fix null coercion

--- library/display/sddm-theme.nix
+++ library/display/sddm-theme.nix
@@ -13,9 +13,11 @@ pkgs.stdenv.mkDerivation {
     mkdir -p $out/share/sddm/themes/SilentSDDM
     cp -R ./* $out/share/sddm/themes/SilentSDDM
 
-    if [ -f "${background}" ]; then
-      cp "${background}" $out/share/sddm/themes/SilentSDDM/backgrounds/custom.jpg
-      sed -i 's/background = smoky.jpg/background = custom.jpg/g' $out/share/sddm/themes/SilentSDDM/configs/default.conf
-    fi
+    ${pkgs.lib.optionalString (background != null) ''
+      if [ -f "${toString background}" ]; then
+        cp "${toString background}" $out/share/sddm/themes/SilentSDDM/backgrounds/custom.jpg
+        sed -i 's/background = smoky.jpg/background = custom.jpg/g' $out/share/sddm/themes/SilentSDDM/configs/default.conf
+      fi
+    ''}
   '';
 }
