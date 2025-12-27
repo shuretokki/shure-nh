bef33393ea396758b2abcad2d3ffbf22b93b764a
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-27 21:39:03 +0700

    add more services

--- library/home/services.nix
+++ library/home/services.nix
@@ -2,6 +2,15 @@
   services.network-manager-applet.enable = true;
   services.blueman-applet.enable = true;
 
+  # Auto-mount disks
+  services.udiskie.enable = true;
+
+  # Audio Effects Daemon
+  services.easyeffects.enable = true;
+
+  # Media Player Daemon
+  services.playerctld.enable = true;
+
   home.packages = with pkgs; [
     pavucontrol
   ];
