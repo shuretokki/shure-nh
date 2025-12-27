b3b1e3da9d0dc29455a78c46a4a1f8d39775d06b
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-27 21:39:34 +0700

    add more services

--- library/home/services.nix
+++ library/home/services.nix
@@ -1,14 +1,8 @@
 { pkgs, ... }: {
   services.network-manager-applet.enable = true;
   services.blueman-applet.enable = true;
-
-  # Auto-mount disks
   services.udiskie.enable = true;
-
-  # Audio Effects Daemon
   services.easyeffects.enable = true;
-
-  # Media Player Daemon
   services.playerctld.enable = true;
 
   home.packages = with pkgs; [
