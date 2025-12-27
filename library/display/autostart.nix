785912cbe877e72c4aa1b4740e33e8ee118b4292
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-27 14:26:14 +0700

    autostart hyprsunset

--- library/display/autostart.nix
+++ library/display/autostart.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   exec-once = [
     "${pkgs.vicinae}/bin/vicinae daemon"
+    "hyprsunset"
     "wl-paste --type text --watch cliphist store"
     "wl-paste --type image --watch cliphist store"
   ];
