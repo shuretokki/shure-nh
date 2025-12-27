c1f18137f1acb32ec72cace7a861ad69ad0fd809
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-27 20:34:58 +0700

    clean hypr env

--- library/display/hyprland/env.nix
+++ library/display/hyprland/env.nix
@@ -4,5 +4,16 @@
     "XCURSOR_THEME,macOS-Monterey"
     "XCURSOR_SIZE,24"
     "HYPRCURSOR_SIZE,24"
+
+    "GDK_BACKEND,wayland,x11,*"
+    "QT_QPA_PLATFORM,wayland;xcb"
+    "QT_STYLE_OVERRIDE,kvantum"
+    "SDL_VIDEODRIVER,wayland"
+    "MOZ_ENABLE_WAYLAND,1"
+    "ELECTRON_OZONE_PLATFORM_HINT,wayland"
+    "OZONE_PLATFORM,wayland"
+    "XDG_SESSION_TYPE,wayland"
+    "XDG_CURRENT_DESKTOP,Hyprland"
+    "XDG_SESSION_DESKTOP,Hyprland"
   ];
 }
