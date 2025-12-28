c13d847a2a8f380313488f0ca46897ed654781d5
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-28 14:02:10 +0700

    update hyprland default to receive and pass vars

--- library/display/hyprland/default.nix
+++ library/display/hyprland/default.nix
@@ -1,11 +1,11 @@
-{ pkgs, ... }:
+{ pkgs, vars, ... }:
 let
   appearance = import ./appearance.nix;
   rules = import ./rules.nix;
   input = import ./input.nix;
   env = import ./env.nix { inherit pkgs; };
   autostart = import ./autostart.nix { inherit pkgs; };
-  keybinds = import ./keybinds.nix { inherit pkgs; };
+  keybinds = import ./keybinds.nix { inherit pkgs vars; };
   mouse-keybinds = import ./mouse-keybinds.nix { inherit pkgs; };
   scripts = import ./scripts.nix { inherit pkgs; };
 in {
