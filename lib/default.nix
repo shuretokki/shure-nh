5216c1f5da4030011c3c9835dfc7e08438372a0a
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-29 22:31:23 +0700

    add mkhost helper

---
+++ lib/default.nix
@@ -0,0 +1,41 @@
+{ inputs }:
+let
+  lib = inputs.nixpkgs.lib;
+  vars = import ../vars.nix;
+in
+{
+  mkHost = {
+    hostname,
+    username,
+    system ? "x86_64-linux",
+    hostVars ? {},
+    extraModules ? []
+  }:
+  let
+    mergedVars = vars // hostVars // { inherit hostname username; };
+  in
+  lib.nixosSystem {
+    inherit system;
+    specialArgs = { inherit inputs; vars = mergedVars; };
+    modules = [
+      ../hosts/${hostname}
+      ../users/${username}/nixos.nix
+
+      inputs.stylix.nixosModules.stylix
+
+      inputs.home-manager.nixosModules.home-manager {
+        home-manager = {
+          useGlobalPkgs = true;
+          useUserPackages = true;
+          extraSpecialArgs = { inherit inputs; vars = mergedVars; };
+          sharedModules = [
+            inputs.vicinae.homeManagerModules.default
+            inputs.stylix.homeManagerModules.stylix
+          ];
+          users.${username} = import ../users/${username}/home.nix;
+          backupFileExtension = "backup";
+        };
+      }
+    ] ++ extraModules;
+  };
+}
