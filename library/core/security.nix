162fd3fbdf8c2cf1bf2b2a844df0c443e9a3e12e
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-29 22:52:45 +0700

    update security config

--- library/core/security.nix
+++ library/core/security.nix
@@ -3,20 +3,4 @@
 
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.login.enableGnomeKeyring = true;
-
-  i18n.inputMethod = {
-    enable = true;
-    type = "fcitx5";
-    fcitx5.waylandFrontend = true;
-    fcitx5.addons = with pkgs; [
-      fcitx5-gtk
-      fcitx5-lua
-    ];
-  };
-
-  # System-wide environment variables
-  # for Fcitx5
-  environment.variables = {
-    XMODIFIERS = "@im=fcitx";
-  };
 }
