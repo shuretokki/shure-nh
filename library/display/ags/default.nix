9653196fcb17640940f059be93984b1e8acce06e
Your Name <your.email@example.com>
2025-12-29 14:50:40 +0700

    fix ags package

--- library/display/ags/default.nix
+++ library/display/ags/default.nix
@@ -10,7 +10,7 @@
       inputs.ags.packages.${pkgs.system}.mpris
       inputs.ags.packages.${pkgs.system}.network
       inputs.ags.packages.${pkgs.system}.battery
-      inputs.ags.packages.${pkgs.system}.wp
+      inputs.ags.packages.${pkgs.system}.wireplumber
       inputs.ags.packages.${pkgs.system}.tray
       gtksourceview
       webkitgtk
