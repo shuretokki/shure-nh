e8168682a1dc5f6121bef49926514af0505725c2
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-29 22:49:25 +0700

    add desktop services

---
+++ library/home/bundlers/desktop/services.nix
@@ -0,0 +1,11 @@
+{ lib, pkgs, ... }: {
+  services.network-manager-applet.enable = lib.mkDefault true;
+  services.blueman-applet.enable = lib.mkDefault true;
+  services.udiskie.enable = lib.mkDefault true;
+  services.easyeffects.enable = lib.mkDefault true;
+  services.playerctld.enable = lib.mkDefault true;
+
+  home.packages = with pkgs; [
+    pavucontrol
+  ];
+}
