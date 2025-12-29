3599eb0097b1a664963e5b59916ab178d6c8d015
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-29 22:58:21 +0700

    update flake inputs

--- flake.nix
+++ flake.nix
@@ -14,14 +14,19 @@
 
     inputs = {
         nixpkgs.url = "nixpkgs/nixos-unstable";
-        hyprland.url = "github:hyprwm/Hyprland";
-        vicinae.url = "github:vicinaehq/vicinae";
 
+        hyprland.url = "github:hyprwm/Hyprland";
         hyprland-plugins = {
             url = "github:hyprwm/hyprland-plugins";
             inputs.hyprland.follows = "hyprland";
         };
 
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+
+        vicinae.url = "github:vicinaehq/vicinae";
         vicinae-extensions = {
             url = "github:vicinaehq/extensions";
             inputs.nixpkgs.follows = "nixpkgs";
@@ -34,17 +39,8 @@
 
         zen-browser = {
             url = "github:0xc000022070/zen-browser-flake";
-            inputs = {
-                # IMPORTANT: we're using "libgbm" and is only available in unstable so ensure
-                # to have it up-to-date or simply don't specify the nixpkgs input
-                nixpkgs.follows = "nixpkgs";
-                home-manager.follows = "home-manager";
-            };
-        };
-
-        home-manager = {
-            url = "github:nix-community/home-manager";
             inputs.nixpkgs.follows = "nixpkgs";
+            inputs.home-manager.follows = "home-manager";
         };
 
         apple-fonts = {
@@ -52,66 +48,51 @@
             inputs.nixpkgs.follows = "nixpkgs";
         };
 
-        ags = {
-            url = "github:Aylur/ags";
+        sops-nix = {
+            url = "github:Mic92/sops-nix";
             inputs.nixpkgs.follows = "nixpkgs";
         };
 
+        stylix = {
+            url = "github:danth/stylix";
+            inputs.nixpkgs.follows = "nixpkgs";
+            inputs.home-manager.follows = "home-manager";
+        };
+
+        nixos-hardware.url = "github:NixOS/nixos-hardware";
+
+        antigravity = {
+            url = "github:jacopone/antigravity-nix";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
 
-    outputs = { self, nixpkgs, home-manager, vicinae, apple-fonts, ... }@inputs:
+    outputs = { self, nixpkgs, ... }@inputs:
     let
-        vars = import ./vars.nix;
-
-        mkSystem = { hostname, username, hostConfigDir, userConfigDir, system ? "x86_64-linux" }:
-            let
-                hostConfig = hostConfigDir + "/default.nix";
-                hardwareConfig = hostConfigDir + "/hardware-configuration.nix";
-                userHomeConfig = userConfigDir + "/home.nix";
-                userNixosConfig = userConfigDir + "/nixos.nix";
-            in
-            assert (builtins.isString hostname && hostname != "");
-            assert (builtins.isString username && username != "");
-
-            if ! (builtins.pathExists hostConfig) then builtins.abort "host config not found at ${hostConfig}"
-            else if ! (builtins.pathExists hardwareConfig) then builtins.abort "hardware config not found at ${hardwareConfig}"
-            else if ! (builtins.pathExists userHomeConfig) then builtins.abort "user home config not found at ${userHomeConfig}"
-            else if ! (builtins.pathExists userNixosConfig) then builtins.abort "user nixos config not found at ${userNixosConfig}"
-            else
-            nixpkgs.lib.nixosSystem {
-                inherit system;
-                specialArgs = { inherit inputs; vars = vars // { inherit hostname username; }; };
-                modules = [
-                    hostConfig
-                    userNixosConfig
-                    home-manager.nixosModules.home-manager {
-                        home-manager = {
-                            useGlobalPkgs = true;
-                            useUserPackages = true;
-                            extraSpecialArgs = { inherit inputs; vars = vars // { inherit hostname username; }; };
-                            sharedModules = [ vicinae.homeManagerModules.default ];
-                            users.${username} = import userHomeConfig;
-                            backupFileExtension = "backup";
-                        };
-                    }
-                ];
-            };
+        libs = import ./lib { inherit inputs; };
     in {
+        formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
+
+        devShells.x86_64-linux.default = let
+            pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in pkgs.mkShell {
+            packages = with pkgs; [ sops age ];
+            shellHook = ''
+                echo "dev shell"
+                echo "sops secrets/secrets.yaml  - Edit secrets"
+            '';
+        };
+
         nixosConfigurations = {
-            # default configuration
-            default = mkSystem {
-                inherit (vars) hostname username;
-                hostConfigDir = ./hosts/desktop;
-                userConfigDir = ./users/shure;
+            default = libs.mkHost {
+                hostname = "desktop";
+                username = "shure";
             };
 
-            # template for new users:
-            # user-pc = mkSystem {
-            #     hostname = "user-pc";
-            #     username = "user";
-            #     hostConfigDir = ./hosts/user-pc;
-            #     userConfigDir = ./users/user;
-            # };
+            desktop = libs.mkHost {
+                hostname = "desktop";
+                username = "shure";
+            };
         };
     };
 }
