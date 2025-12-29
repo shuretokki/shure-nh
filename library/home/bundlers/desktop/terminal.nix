ab20ec8716655cf076ccf27ba52e8c4ab29871a7
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-29 22:46:55 +0700

    add desktop terminal

---
+++ library/home/bundlers/desktop/terminal.nix
@@ -0,0 +1,25 @@
+{ lib, pkgs, vars, ... }: {
+  home.packages = with pkgs; [
+    warp-terminal
+    ghostty
+  ];
+
+  programs.kitty = {
+    enable = lib.mkDefault true;
+    settings = {
+      font_family = lib.mkDefault vars.fontMono;
+      font_size = lib.mkDefault vars.fontSize;
+      enable_audio_bell = false;
+    };
+  };
+
+  programs.alacritty = {
+    enable = lib.mkDefault true;
+    settings = {
+      font.normal.family = lib.mkDefault vars.fontMono;
+      font.size = lib.mkDefault vars.fontSize;
+    };
+  };
+
+  home.sessionVariables.TERMINAL = lib.mkDefault vars.terminal;
+}
