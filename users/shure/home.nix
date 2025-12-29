177a5198425b4b5dd5f0c062dd8f03dec900ae60
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-29 22:51:11 +0700

    simplify user config

--- users/shure/home.nix
+++ users/shure/home.nix
@@ -1,17 +1,7 @@
 { config, pkgs, inputs, vars, ... }: {
     imports = [
+        ../../library/home/global
         ../../library/home/vicinae.nix
-        ../../library/home/git.nix
-        ../../library/home/vscode.nix
-        ../../library/home/theming.nix
-        ../../library/home/services.nix
-        ../../library/home/music.nix
-        ../../library/home/browser.nix
-        ../../library/home/terminal.nix
-        ../../library/home/shell.nix
-        ../../library/home/apps.nix
-        ../../library/home/variables.nix
-        ../../library/home/mime.nix
     ];
 
     home.username = vars.username;
