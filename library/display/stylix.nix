2807302c45c2e992a3183b33dbf8f3cad7ee00a9
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-30 17:55:37 +0700

    use local wallpaper

--- library/display/stylix.nix
+++ library/display/stylix.nix
@@ -5,10 +5,7 @@
 
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
 
-    image = pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/AngelJumbo/gruvbox-wallpapers/refs/heads/main/wallpapers/mix/gruvbox_scenery.png";
-      sha256 = "07187428f731e84764836ac4288012674209581895a9829281519ef16fb41e4d";
-    };
+    image = ./wallpapers/gruvbox_scenery.png;
 
     polarity = "dark";
 
