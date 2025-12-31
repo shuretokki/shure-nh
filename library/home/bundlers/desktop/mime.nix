3f98879d397d2c341419f6c311b677acc9b1166f
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-31 22:00:30 +0700

    fix zen browser default

--- library/home/bundlers/desktop/mime.nix
+++ library/home/bundlers/desktop/mime.nix
@@ -1,12 +1,15 @@
-{ lib, vars, ... }: {
+{ lib, vars, ... }:
+let
+  browserDesktop = if vars.browser == "zen" then "zen-beta" else vars.browser;
+in {
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
-      "text/html" = lib.mkForce "${vars.browser}.desktop";
-      "x-scheme-handler/http" = lib.mkForce "${vars.browser}.desktop";
-      "x-scheme-handler/https" = lib.mkForce "${vars.browser}.desktop";
-      "x-scheme-handler/about" = lib.mkForce "${vars.browser}.desktop";
-      "x-scheme-handler/unknown" = lib.mkForce "${vars.browser}.desktop";
+      "text/html" = lib.mkForce "${browserDesktop}.desktop";
+      "x-scheme-handler/http" = lib.mkForce "${browserDesktop}.desktop";
+      "x-scheme-handler/https" = lib.mkForce "${browserDesktop}.desktop";
+      "x-scheme-handler/about" = lib.mkForce "${browserDesktop}.desktop";
+      "x-scheme-handler/unknown" = lib.mkForce "${browserDesktop}.desktop";
     };
   };
 }
