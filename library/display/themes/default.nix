c92a4cb44980ca3972039411c040dede6a544a53
Tri R. Utomo <tri.r.utomo@gmail.com>
2026-01-01 19:58:50 +0700

    add theme options

---
+++ library/display/themes/default.nix
@@ -0,0 +1,128 @@
+{ lib, pkgs, ... }:
+let
+  inherit (lib) mkOption types;
+in {
+  options.theme = {
+    scheme = mkOption {
+      type = types.path;
+      description = "Path to base16 scheme yaml file";
+      default = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
+    };
+
+    polarity = mkOption {
+      type = types.enum [ "dark" "light" ];
+      default = "dark";
+      description = "Theme polarity";
+    };
+
+    fonts = {
+      mono = mkOption { type = types.str; default = "JetBrainsMono Nerd Font"; };
+      sans = mkOption { type = types.str; default = "SF Pro Rounded"; };
+      size = mkOption { type = types.int; default = 12; };
+    };
+
+    cursor = {
+      name = mkOption { type = types.str; default = "macOS"; };
+      size = mkOption { type = types.int; default = 24; };
+    };
+
+    wallpaper = mkOption {
+      type = types.path;
+      description = "Path to the wallpaper file";
+      default = "";
+    };
+
+    grub = {
+      enable = mkOption {
+        type = types.bool;
+        default = true;
+        description = "Enable custom GRUB theming";
+      };
+
+      theme = mkOption {
+        type = types.package;
+        description = "GRUB theme package to use";
+        default = pkgs.stdenv.mkDerivation {
+          pname = "wuthering-grub-theme";
+          version = "1.0";
+          src = pkgs.fetchFromGitHub {
+              owner = "vinceliuice";
+              repo = "Wuthering-grub2-themes";
+              rev = "ed3f8bcd292e7a0684f3c30f20939710d263a321";
+              sha256 = "sha256-q9TLZTZI/giwKu8sCTluxvkBG5tyan7nFOqn4iGLnkA=";
+          };
+          installPhase = ''
+              mkdir -p $out
+              cp -a $src/common/*.pf2 $out/
+              cp -a $src/config/theme-1080p.txt $out/theme.txt
+              cp -a $src/backgrounds/background-jinxi.jpg $out/background.jpg
+              cp -a $src/assets/assets-icons/icons-1080p $out/icons
+              cp -a $src/assets/assets-other/other-1080p/*.png $out/
+          '';
+        };
+      };
+    };
+
+    sddm = {
+      enable = mkOption {
+        type = types.bool;
+        default = true;
+        description = "Enable custom SDDM theming";
+      };
+
+      theme = mkOption {
+        type = types.package;
+        description = "SDDM theme package to use";
+        default = pkgs.stdenv.mkDerivation {
+          name = "silent-sddm";
+          src = pkgs.fetchFromGitHub {
+            owner = "uiriansan";
+            repo = "SilentSDDM";
+            rev = "3705a132db1e101a5ec2aa14b0e28e8ccd78866a";
+            hash = "sha256-znjp0gAxt+1wkxp/rqc0NPAnQGikbCAylgWGussZj0I=";
+          };
+          installPhase = ''
+            mkdir -p $out/share/sddm/themes/SilentSDDM
+            cp -R ./* $out/share/sddm/themes/SilentSDDM
+            # Simple install, actual config might need font replacement in theme definition where used
+          '';
+        };
+      };
+    };
+
+    waybar = {
+      style = mkOption {
+        type = types.lines;
+        default = "";
+        description = "CSS content for Waybar";
+      };
+
+      config = mkOption {
+        type = types.attrs;
+        default = {};
+        description = "Configuration attributes for Waybar";
+      };
+    };
+
+    hyprland = {
+      gaps-in = mkOption { type = types.int; default = 4; };
+      gaps-out = mkOption { type = types.int; default = 4; };
+      rounding = mkOption { type = types.int; default = 0; };
+      blur = mkOption { type = types.bool; default = true; };
+      shadows = mkOption { type = types.bool; default = true; };
+      active-border-col = mkOption { type = types.str; default = "rgba(33ccffee) rgba(00ff99ee) 45deg"; };
+      inactive-border-col = mkOption { type = types.str; default = "rgba(595959aa)"; };
+    };
+
+    hyprlock = {
+      fontFamily = mkOption { type = types.str; default = "SF Pro Rounded"; };
+      fontSize = mkOption { type = types.int; default = 64; };
+      input-field = {
+        size = mkOption { type = types.str; default = "300, 50"; };
+        outline_thickness = mkOption { type = types.int; default = 2; };
+        dots_size = mkOption { type = types.float; default = 0.33; };
+        dots_spacing = mkOption { type = types.float; default = 0.15; };
+      };
+    };
+  };
+}
