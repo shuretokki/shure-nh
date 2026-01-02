a0500c0284bc966cecfe455636fbf47cf2b29723
Tri R. Utomo <tri.r.utomo@gmail.com>
2026-01-02 12:57:18 +0700

    nh config

--- library/core/system.nix
+++ library/core/system.nix
@@ -28,7 +28,12 @@
   programs.dconf.enable = true;
   services.gvfs.enable = true;
   services.udisks2.enable = true;
-  programs.ydotool.enable = true;
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/home/${vars.username}/shuredotnix";
+  };
 
   xdg.portal = {
     enable = true;
