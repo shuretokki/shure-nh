ed0715b72b8a2d16d5b2efc042d5bad09d97d4e5
Tri R. Utomo <tri.r.utomo@gmail.com>
2026-01-02 23:38:30 +0700

    remove additional substituters and trusted key

--- library/core/system.nix
+++ library/core/system.nix
@@ -5,15 +5,6 @@
   nix.settings = {
     auto-optimise-store = true;
     experimental-features = ["nix-command" "flakes"];
-    substituters = [
-      "https://hyprland.cachix.org"
-      "https://vicinae.cachix.org"
-    ];
-    trusted-substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = [
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
-    ];
   };
 
   networking.hostName = vars.hostname;
