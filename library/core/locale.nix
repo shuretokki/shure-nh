5e793509dcfeb9c1f4e06152589daac542abf6fb
Tri R. Utomo <tri.r.utomo@gmail.com>
2026-01-02 14:24:39 +0700

    proper locale handling

--- library/core/locale.nix
+++ library/core/locale.nix
@@ -1,15 +1,15 @@
-{ lib, ... }: {
-  time.timeZone = lib.mkDefault "Asia/Jakarta";
-  i18n.defaultLocale = lib.mkDefault "en_US.UTF-8";
+{ lib, vars, ... }: {
+  time.timeZone = lib.mkDefault (vars.timezone or "Asia/Jakarta");
+  i18n.defaultLocale = lib.mkDefault (vars.locale or "en_US.UTF-8");
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = lib.mkDefault "en_US.UTF-8";
-    LC_IDENTIFICATION = lib.mkDefault "en_US.UTF-8";
-    LC_MEASUREMENT = lib.mkDefault "en_US.UTF-8";
-    LC_MONETARY = lib.mkDefault "en_US.UTF-8";
-    LC_NAME = lib.mkDefault "en_US.UTF-8";
-    LC_NUMERIC = lib.mkDefault "en_US.UTF-8";
-    LC_PAPER = lib.mkDefault "en_US.UTF-8";
-    LC_TELEPHONE = lib.mkDefault "en_US.UTF-8";
-    LC_TIME = lib.mkDefault "en_US.UTF-8";
+    LC_ADDRESS = lib.mkDefault (vars.locale or "en_US.UTF-8");
+    LC_IDENTIFICATION = lib.mkDefault (vars.locale or "en_US.UTF-8");
+    LC_MEASUREMENT = lib.mkDefault (vars.locale or "en_US.UTF-8");
+    LC_MONETARY = lib.mkDefault (vars.locale or "en_US.UTF-8");
+    LC_NAME = lib.mkDefault (vars.locale or "en_US.UTF-8");
+    LC_NUMERIC = lib.mkDefault (vars.locale or "en_US.UTF-8");
+    LC_PAPER = lib.mkDefault (vars.locale or "en_US.UTF-8");
+    LC_TELEPHONE = lib.mkDefault (vars.locale or "en_US.UTF-8");
+    LC_TIME = lib.mkDefault (vars.locale or "en_US.UTF-8");
   };
 }
