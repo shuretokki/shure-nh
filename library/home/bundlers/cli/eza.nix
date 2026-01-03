1fc22ae0611ef61ddfee5ad2acdb868ace9d1d0a
Tri R. Utomo <tri.r.utomo@gmail.com>
2026-01-03 19:30:46 +0700

    update eza config

--- library/home/bundlers/cli/eza.nix
+++ library/home/bundlers/cli/eza.nix
@@ -1,9 +1,22 @@
-{ lib, ... }: {
+# https://github.com/nix-community/home-manager/blob/master/modules/programs/eza.nix
+
+{
+  lib,
+  ...
+}:
+{
   programs.eza = {
     enable = lib.mkDefault true;
-    enableBashIntegration = lib.mkDefault true;
-    icons = lib.mkDefault "auto";
-    git = lib.mkDefault true;
-    extraOptions = [ "--group-directories-first" ];
+    enableBashIntegration = true;
+
+    icons = "auto";
+    colors = "auto";
+    git = true;
+    extraOptions = [
+      "--group-directories-first"
+      "--header"
+    ];
+
+    theme = { };
   };
 }
