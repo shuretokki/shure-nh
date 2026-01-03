497da64d35e8b726312e4d88383e7b2ee608abde
Tri R. Utomo <tri.r.utomo@gmail.com>
2026-01-03 19:28:09 +0700

    update fzf config

--- library/home/bundlers/cli/fzf.nix
+++ library/home/bundlers/cli/fzf.nix
@@ -1,7 +1,59 @@
-{ lib, ... }: {
+# https://github.com/nix-community/home-manager/blob/master/modules/programs/fzf.nix
+
+{
+  lib,
+  config,
+  ...
+}:
+let
+  c = config.lib.stylix.colors.withHashtag;
+in
+{
   programs.fzf = {
     enable = lib.mkDefault true;
-    enableBashIntegration = lib.mkDefault true;
-    defaultOptions = [ "--height 40%" "--layout=reverse" "--border" ];
+    enableBashIntegration = true;
+
+    defaultCommand = "fd --type f --hidden --follow --exclude .git";
+    defaultOptions = [
+      "--height 40%"
+      "--layout=reverse"
+      "--border"
+      "--info=inline"
+    ];
+
+    fileWidgetCommand = "fd --type f --hidden --follow --exclude .git";
+    fileWidgetOptions = [
+      "--preview 'head -100 {}'"
+    ];
+
+    changeDirWidgetCommand = "fd --type d --hidden --follow --exclude .git";
+    changeDirWidgetOptions = [
+      "--preview 'eza --tree --level=2 --color=always {} | head -50'"
+    ];
+
+    historyWidgetOptions = [
+      "--sort"
+      "--exact"
+    ];
+
+    colors = {
+      "bg" = c.base00;
+      "bg+" = c.base01;
+      "fg" = c.base05;
+      "fg+" = c.base06;
+      "hl" = c.base0D;
+      "hl+" = c.base0D;
+      "info" = c.base0A;
+      "marker" = c.base0B;
+      "prompt" = c.base0E;
+      "spinner" = c.base0C;
+      "pointer" = c.base0E;
+      "header" = c.base0D;
+    };
+
+    tmux = {
+      enableShellIntegration = false;
+      shellIntegrationOptions = [ ];
+    };
   };
 }
